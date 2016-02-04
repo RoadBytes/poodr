c5f676e509d0d31101e3008a066f0639061724e9
Jason Data <jason.data@roadbytes.me>
2016-02-03 17:44:08 -0800

    added argument order exercises

---
+++ Ch3practice/RemoveArgumentOrder/book_solution_fetch.rb
@@ -0,0 +1,32 @@
+class Gear
+  attr_reader :chainring, :cog, :wheel
+  def initialize( args )
+    @chainring = args.fetch( :chainring, 40 )
+    @cog       = args.fetch( :cog, 18 )
+    @wheel     = args.fetch( :wheel )
+  end
+
+  def ratio 
+    chainring / cog.to_f 
+  end 
+
+  def gear_inches
+    ratio * diameter
+  end
+
+  def diameter
+    wheel.diameter
+  end
+end
+
+class Wheel 
+  attr_reader :rim, :tire 
+  def initialize( rim, tire) 
+    @rim  = rim 
+    @tire = tire 
+  end 
+  
+  def diameter 
+    rim + (tire * 2) 
+  end 
+end
---
+++ Ch3practice/RemoveArgumentOrder/test_remove_argument_order.rb
@@ -0,0 +1,17 @@
+require 'minitest/autorun'
+require_relative 'remove_argument_order'
+
+class GearTest < Minitest::Test
+  def test_gear_ratio
+    gear  = Gear.new(chainring: 52, cog: 11, wheel: Wheel.new( 1, 1))
+    ratio = 52 / 11.to_f
+    assert_equal ratio, gear.ratio
+  end
+
+  def test_gear_inches
+    gear  = Gear.new(chainring: 52, cog: 11, wheel: Wheel.new( 1, 1))
+    inches = (52 / 11.to_f) * (1 + (2 * 1))
+    assert_equal inches, gear.gear_inches
+  end
+end
+
